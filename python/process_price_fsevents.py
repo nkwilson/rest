6fbf567d8a12108441250ba35fa7eb784ce6ba7b
nkwilson <nkwilson@sina.com>
2018-08-06 07:32:06 +0800

    try to use pandas to calculate bollinger band

--- python/process_price_fsevents.py
+++ python/process_price_fsevents.py
@@ -1,10 +1,25 @@
 import sys
+#import pandas
+#import numpy
 from fsevents import Observer
 
 print (sys.argv)
 
 observer = Observer()
 observer.start()
+
+# close_prices = pandas.Series()
+
+# parameters for bollinger band
+window_size=20 
+num_std=2
+
+# #import the pandas library and aliasing as pd
+# import pandas as pd
+# import numpy as np
+# data = np.array(['a','b','c','d'])
+# s = pd.Series(data,index=[100,101,102,103])
+# print s
 
 def callback_dir(subpath, mask):
     print (subpath, mask)
@@ -17,9 +32,33 @@ def callback_file(subpath):
     #print (type(tup), tup[0])
     event_type=tup[0]
     event_path=tup[2]
-    if (event_type == 256):
-        print (event_type, event_path)
-              
+    if (event_type != 256):
+        return
+    # if (event_type == 2):
+        # with open(event_path, 'r') as f:
+        #     close=eval(f.readline())[3]
+        # print (close, close_prices)
+        # try:
+        #     # Parameters:	
+        #     # to_append : Series or list/tuple of Series
+        #     # ignore_index : boolean, default False
+        #     # If True, do not use the index labels.
+        #     # New in version 0.19.0.
+        #     # verify_integrity : boolean, default False
+        #     # If True, raise Exception on creating index with duplicates
+            
+        #     # Series.append(to_append, ignore_index=False, verify_integrity=False)[source]
+        #     # Concatenate two or more Series.
+        #     close_prices[os.path.basename(event_path)]=close
+        #     return
+        # except Exception as ex:
+        #     # not exist
+        #     close_prices.append(pandas.Series([close], [os.path.basename(event_path)]), verify_integrity=True)
+        #     return
+    # elif (event_type != 256):
+    #     return
+    print (event_type, event_path)
+    # close_prices.append(pandas.Series([close], [os.path.basename(event_path)]), verify_integrity=True)    
 
 from fsevents import Stream
 stream = Stream(callback_file, sys.argv[1], file_events=True)
