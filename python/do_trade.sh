1471bd2a0bfb3d461514c68f75f465b7df5242aa
nkwilson <nkwilson@sina.com>
2018-09-19 00:04:51 +0800

    use monitor_me to start py

--- python/do_trade.sh
+++ python/do_trade.sh
@@ -12,14 +12,14 @@ expr "$2" + "0" || (echo  "Invalid amount $2" && false)
 
 if ps U $(whoami) | grep -qv 'python3 price_notify.py';  then
 	pushd $(dirname ${target_coin})
-	python3 price_notify.py > /dev/null 2>&1 &
+	python3 monitor_me.py price_notify.py > /dev/null 2>&1 &
 	popd
 fi	
 
 echo "${amount}" > ${target_coin_amount}
-(python3 trade.py ${target_coin} >> ${target_coin}.trade.log) 2>&1 &
+(python3 monitor_me.py trade.py ${target_coin} >> ${target_coin}.trade.log) 2>&1 &
 sleep 10 
-(python3 trade_notify.py ${target_coin} >> ${target_coin}.trade_notify.log ) 2>&1 &
+(python3 monitor_me.py trade_notify.py ${target_coin} >> ${target_coin}.trade_notify.log ) 2>&1 &
 sleep 10
-(python3 boll_notify.py ${target_coin} with-old-files >> ${target_coin}.boll_notify.log ) 2>&1 &
+(python3 monitor_me.py boll_notify.py ${target_coin} with-old-files >> ${target_coin}.boll_notify.log ) 2>&1 &
 
