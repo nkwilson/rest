eb8568321684055fb968aafe1573f34c9f8ae3be
nkwilson <nkwilson@sina.com>
2018-09-25 23:26:46 +0800

    reduce amount to 1/3

--- python/trade.py
+++ python/trade.py
@@ -294,7 +294,7 @@ def do_trade_new(subpath):
             if balance > 0 and last_bond > 0: # successed
                 print ('balance is updated from %f to %f\n' % (last_balance, balance))
                 last_balance = balance
-                new_amount = int(last_balance / bond)
+                new_amount = int(last_balance / bond / 3)
                 if new_amount != amount:
                     print ('amount is updated from %d to %d, with bond %f\n' % (amount, new_amount, last_bond))
                     amount = new_amount
