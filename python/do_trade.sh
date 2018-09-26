7623cd260d402e4377edb10a5b854e4d97580d60
nkwilson <nkwilson@sina.com>
2018-09-26 22:47:21 +0800

    fix check logic

--- python/do_trade.sh
+++ python/do_trade.sh
@@ -10,10 +10,12 @@ amount=${2:-15}
 test -d ${target_coin} || (echo "Invalid coin $1" && false)
 expr "$2" + "0" || (echo  "Invalid amount $2" && false)
 
-if ps U $(whoami) | grep -qv 'python3 price_notify.py';  then
-	pushd $(dirname ${target_coin})
-	python3 monitor_me.py price_notify.py > /dev/null 2>&1 &
-	popd
+if ps U $(whoami) | grep -q 'python3 price_notify.py';  then
+    true
+else
+    pushd $(dirname ${target_coin})
+    python3 monitor_me.py price_notify.py > /dev/null 2>&1 &
+    popd
 fi	
 
 echo "${amount}" > ${target_coin_amount}
