aa95b1a3d03cd1c738c44a6587dc1f5201d15ae0
yubin <yubin@h3c.com>
2018-12-11 08:54:52 +0800

    adjust to use new cmp_scale option and ratio between different klines

--- python/boll-cascade.sh
+++ python/boll-cascade.sh
@@ -8,9 +8,9 @@ KEY3=${5:-5min}
 SYMBOL1=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY1}
 SYMBOL2=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY2}
 SYMBOL3=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY3}
-RATE1=1
+RATE1=4
 RATE2=2
-RATE3=4
+RATE3=1
 DIVID=7
 echo "start trade on symbol"
 echo "  ${SYMBOL1}"
@@ -23,12 +23,15 @@ case ${COIN} in
      btc)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
 	 echo '0.012' > ${SYMBOL1}.boll_fee
+	 SCALE1=1
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
 	 echo '0.012' > ${SYMBOL2}.boll_fee
-
+	 SCALE2=1
+	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount	 
 	 echo '0.012' > ${SYMBOL3}.boll_fee
+	 SCALE3=1
 	 ;;
      eth)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
@@ -54,21 +57,21 @@ esac
 	 
 python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1} &
 sleep 2
-python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} &
+python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} --cmp_scale=${SCALE1} &
 sleep 2
 python3 monitor_me.py trade.py --signal=boll ${SYMBOL1} &
 sleep 2
 
 python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL2} &
 sleep 2
-python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL2} --startup_notify=${SYMBOL1}.boll_trade.startup --shutdown_notify=${SYMBOL1}.boll_trade.shutdown &
+python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL2} --cmp_scale=${SCALE2} --startup_notify=${SYMBOL1}.boll_trade.startup --shutdown_notify=${SYMBOL1}.boll_trade.shutdown &
 sleep 2
 python3 monitor_me.py trade.py --signal=boll ${SYMBOL2} &
 sleep 2
 
 python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL3} &
 sleep 2
-python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL3} --startup_notify=${SYMBOL2}.boll_trade.startup --shutdown_notify=${SYMBOL2}.boll_trade.shutdown &
+python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL3} --cmp_scale=${SCALE3} --startup_notify=${SYMBOL2}.boll_trade.startup --shutdown_notify=${SYMBOL2}.boll_trade.shutdown &
 sleep 2
 python3 monitor_me.py trade.py --signal=boll ${SYMBOL3} &
 sleep 2
