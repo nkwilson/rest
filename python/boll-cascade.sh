f1bc006b7fc132ac4bda8219aedf76a2469a4ca0
yubin <yubin@h3c.com>
2018-12-13 13:12:11 +0800

    descrease sense of eth to scale=10

--- python/boll-cascade.sh
+++ python/boll-cascade.sh
@@ -44,19 +44,19 @@ case ${COIN} in
      eth)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
 	 echo '0.00012' > ${SYMBOL1}.boll_fee
-	 SCALE1=100
+	 SCALE1=10
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
 	 echo '0.00012' > ${SYMBOL2}.boll_fee
-	 SCALE2=100
+	 SCALE2=10
 	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount
 	 echo '0.00012' > ${SYMBOL3}.boll_fee
-	 SCALE3=100
+	 SCALE3=10
 	 
 	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount
 	 echo '0.00012' > ${SYMBOL4}.boll_fee
-	 SCALE4=100
+	 SCALE4=10
 	 ;;
      ltc)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
