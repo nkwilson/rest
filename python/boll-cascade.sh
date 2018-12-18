17cdcf907d32e5e1e5421e8428cc39e1850eb6ce
yubin <yubin@h3c.com>
2018-12-18 08:59:54 +0800

    missing eos case

--- python/boll-cascade.sh
+++ python/boll-cascade.sh
@@ -75,6 +75,26 @@ case ${COIN} in
 	 echo '0.00012' > ${SYMBOL4}.boll_fee
 	 SCALE4=100
 	 ;;
+     eos)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo '0.00012' > ${SYMBOL1}.boll_fee
+	 SCALE1=1000
+	 
+	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
+	 echo '0.00012' > ${SYMBOL2}.boll_fee
+	 SCALE2=1000
+	 
+	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount
+	 echo '0.00012' > ${SYMBOL3}.boll_fee
+	 SCALE3=1000
+	 
+	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount
+	 echo '0.00012' > ${SYMBOL4}.boll_fee
+	 SCALE4=1000
+	 ;;
+     *)
+	 echo 'unknow coin, quit'
+	 exit 
 esac
 	 
 python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1} &
