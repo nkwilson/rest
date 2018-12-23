874effe6f05aadeeb63b1d90a1c9dbf1b41adeb5
nkwilson <nkwilson@sina.com>
2018-12-23 21:50:38 +0800

    update ratio and fee

--- python/boll-cascade.sh
+++ python/boll-cascade.sh
@@ -1,7 +1,7 @@
 set -e
 
 COIN=${1:-btc}
-TOTAL=${2:-10} # 3:3:3:3 ratio
+TOTAL=${2:-48} # 3:5:9:15 ratio
 KEY1=${3:-1hour}
 KEY2=${4:-30min}
 KEY3=${5:-5min}
@@ -11,10 +11,14 @@ SYMBOL2=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY2}
 SYMBOL3=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY3}
 SYMBOL4=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY4}
 RATE1=3
-RATE2=3
-RATE3=3
-RATE4=3
-DIVID=12
+RATE2=5
+RATE3=9
+RATE4=15
+DIVID=48  # first is 10/15 in trading
+FEE_RATE1=0.001
+FEE_RATE2=0.001
+FEE_RATE3=0.001
+FEE_RATE4=0.001
 echo "start trade on symbol"
 echo "  ${SYMBOL1}"
 echo "  ${SYMBOL2}"
@@ -24,79 +28,82 @@ echo "  ${SYMBOL4}"
 TIMES=1
 
 case ${COIN} in
-     btc)
+     btc|dry)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo '0.001' > ${SYMBOL1}.boll_fee
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=1
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
-	 echo '0.001' > ${SYMBOL2}.boll_fee
+	 echo "${FEE_RATE2}" > ${SYMBOL2}.boll_fee
 	 SCALE2=1
 	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount	 
-	 echo '0.001' > ${SYMBOL3}.boll_fee
+	 echo "${FEE_RATE3}" > ${SYMBOL3}.boll_fee
 	 SCALE3=1
 
 	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount	 
-	 echo '0.001' > ${SYMBOL4}.boll_fee
+	 echo "${FEE_RATE4}" > ${SYMBOL4}.boll_fee
 	 SCALE4=1
 	 ;;
      eth)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo '0.001' > ${SYMBOL1}.boll_fee
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=100
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
-	 echo '0.001' > ${SYMBOL2}.boll_fee
+	 echo "${FEE_RATE2}" > ${SYMBOL2}.boll_fee
 	 SCALE2=100
 	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount
-	 echo '0.001' > ${SYMBOL3}.boll_fee
+	 echo "${FEE_RATE3}" > ${SYMBOL3}.boll_fee
 	 SCALE3=100
 	 
 	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount
-	 echo '0.001' > ${SYMBOL4}.boll_fee
+	 echo "${FEE_RATE4}" > ${SYMBOL4}.boll_fee
 	 SCALE4=100
 	 ;;
      ltc)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo '0.001' > ${SYMBOL1}.boll_fee
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=100
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
-	 echo '0.001' > ${SYMBOL2}.boll_fee
+	 echo "${FEE_RATE2}" > ${SYMBOL2}.boll_fee
 	 SCALE2=100
 	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount	 
-	 echo '0.001' > ${SYMBOL3}.boll_fee
+	 echo "${FEE_RATE3}" > ${SYMBOL3}.boll_fee
 	 SCALE3=100
 
 	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount	 
-	 echo '0.001' > ${SYMBOL4}.boll_fee
+	 echo "${FEE_RATE4}" > ${SYMBOL4}.boll_fee
 	 SCALE4=100
 	 ;;
      eos)
 	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo '0.001' > ${SYMBOL1}.boll_fee
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=1000
 	 
 	 echo $(expr "${RATE2}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL2}.boll_amount
-	 echo '0.001' > ${SYMBOL2}.boll_fee
+	 echo "${FEE_RATE2}" > ${SYMBOL2}.boll_fee
 	 SCALE2=1000
 	 
 	 echo $(expr "${RATE3}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL3}.boll_amount
-	 echo '0.001' > ${SYMBOL3}.boll_fee
+	 echo "${FEE_RATE3}" > ${SYMBOL3}.boll_fee
 	 SCALE3=1000
 	 
 	 echo $(expr "${RATE4}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL4}.boll_amount
-	 echo '0.001' > ${SYMBOL4}.boll_fee
+	 echo "${FEE_RATE4}" > ${SYMBOL4}.boll_fee
 	 SCALE4=1000
 	 ;;
      *)
 	 echo 'unknow coin, quit'
 	 exit 
 esac
-	 
+
+# quit if dry
+test "${COIN}" = "dry" && exit
+
 python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1} &
 sleep 2
 python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} --cmp_scale=${SCALE1} &
