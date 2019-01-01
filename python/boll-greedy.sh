5bfbc31ef059d3137c57140c781951ea791ba5a3
nkwilson <nkwilson@sina.com>
2019-01-01 10:24:21 +0800

    Create boll-greedy.sh

---
+++ python/boll-greedy.sh
@@ -0,0 +1,54 @@
+set -e
+
+COIN=${1:-btc}
+TOTAL=${2:-50} # 3:3:3:3 ratio
+KEY1=${3:-30min}
+SYMBOL1=../../websocket/python/ok_sub_futureusd_${COIN}_kline_quarter_${KEY1}
+RATE1=1
+DIVID=50
+FEE_RATE1=0.001
+echo "start trade on symbol"
+echo "  ${SYMBOL1}"
+
+TIMES=1
+
+case ${COIN} in
+     btc|dry)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 SCALE1=1
+	 ;;
+     bch)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 SCALE1=1
+	 ;;
+     eth)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 SCALE1=100
+	 ;;
+     ltc)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 SCALE1=100
+	 ;;
+     eos)
+	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 SCALE1=1000
+	 ;;
+     *)
+	 echo 'unknow coin, quit'
+	 exit 
+esac
+
+# quit if dry
+test "${COIN}" = "dry" && exit
+
+jobs -x python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1}
+fswatch -1 ${SYMBOL1}.boll_notify.ok
+jobs -x python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} --cmp_scale=${SCALE1}
+fswatch -1 ${SYMBOL1}.boll_trade_notify.ok
+jobs -x python3 monitor_me.py trade.py --signal=boll ${SYMBOL1}
+
