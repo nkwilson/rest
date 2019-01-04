c68ce6b2fe43a7ca06ead1a7ac526a94d7f65f79
nkwilson <nkwilson@sina.com>
2019-01-04 23:22:48 +0800

    should use '&' with jobs command

--- python/boll-greedy.sh
+++ python/boll-greedy.sh
@@ -14,28 +14,28 @@ TIMES=1
 
 case ${COIN} in
      btc|dry)
-	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 #echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 #echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=1
 	 ;;
      bch)
-	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 #echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 #echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=1
 	 ;;
      eth)
-	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 #echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 #echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=100
 	 ;;
      ltc)
-	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 #echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 #echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=100
 	 ;;
      eos)
-	 echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
-	 echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
+	 #echo $(expr "${RATE1}" '*' "${TOTAL}" '/' "${DIVID}" '*' $"${TIMES}") > ${SYMBOL1}.boll_amount
+	 #echo "${FEE_RATE1}" > ${SYMBOL1}.boll_fee
 	 SCALE1=1000
 	 ;;
      *)
@@ -46,9 +46,9 @@ esac
 # quit if dry
 test "${COIN}" = "dry" && exit
 
-jobs -x python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1}
+jobs -x python3 monitor_me.py signal_notify.py --signal=boll --dir=${SYMBOL1} &
 fswatch -1 ${SYMBOL1}.boll_notify.ok
-jobs -x python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} --cmp_scale=${SCALE1}
+jobs -x python3 monitor_me.py trade_notify.py --signal=boll --dir=${SYMBOL1} --cmp_scale=${SCALE1} &
 fswatch -1 ${SYMBOL1}.boll_trade_notify.ok
-jobs -x python3 monitor_me.py trade.py --signal=boll ${SYMBOL1}
+jobs -x python3 monitor_me.py trade.py --signal=boll ${SYMBOL1} &
 
