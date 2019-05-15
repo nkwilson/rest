e7f494c3b32afe6e52bc889db3581b4764a4b972
nkwilson <nkwilson@sina.com>
2019-05-15 08:47:16 +0800

    embedded trade_elite.py into it

--- python/trade_notify-all_in_one.py
+++ python/trade_notify-all_in_one.py
@@ -40,7 +40,207 @@ limit_price = 0
 limit_symbol = ''
 limit_amount = 0
 
-import trade_elite
+#import trade_elite
+import json
+
+#初始化apikey，secretkey,url
+#apikey = 'd8da16f9-a531-4853-b9ee-ab07927c4fef'
+#secretkey = '4752BE55655A6233A7254628FB7E9F50'
+#okcoinRESTURL = 'www.okcoin.com'   #请求注意：国内账号需要 修改为 www.okcoin.cn
+#apikey = 'fd07e7f8-519a-4ba3-8289-af9881181a96'
+#secretkey = 'E0EACE4FB14A8CBDFE271E87D6FA2B6C'
+#okcoinRESTURL = 'www.okcoin.cn'
+apikey = 'e2625f5d-6227-4cfd-9206-ffec43965dab'
+secretkey = "27BD16FD606625BCD4EE6DCA5A8459CE"
+okcoinRESTURL = 'www.okex.com'
+
+alt_apikey = ''
+alt_secretkey = ''
+alt_okcoinRESTRUL = ''
+
+#现货API
+okcoinSpot = OKCoinSpot(okcoinRESTURL,apikey,secretkey)
+
+#期货API
+okcoinFuture = OKCoinFuture(okcoinRESTURL,apikey,secretkey)
+
+#print (u'K线信息')
+# print (okcoinSpot.kline('btc_cny'))
+
+#print (u' 现货行情 ')
+#print (okcoinSpot.ticker('btc_usd'))
+
+#print (u' 现货深度 ')
+#print (okcoinSpot.depth('btc_usd'))
+
+#print (u' 现货历史交易信息 ')
+#print (okcoinSpot.trades())
+
+#print (u' 用户现货账户信息 ')
+#print (okcoinSpot.userinfo())
+
+#print (u' 现货下单 ')
+#print (okcoinSpot.trade('ltc_usd','buy','0.1','0.2'))
+
+#print (u' 现货批量下单 ')
+#print (okcoinSpot.batchTrade('ltc_usd','buy','[{price:0.1,amount:0.2},{price:0.1,amount:0.2}]'))
+
+#print (u' 现货取消订单 ')
+#print (okcoinSpot.cancelOrder('ltc_usd','18243073'))
+
+#print (u' 现货订单信息查询 ')
+#print (okcoinSpot.orderinfo('ltc_usd','18243644'))
+
+#print (u' 现货批量订单信息查询 ')
+#print (okcoinSpot.ordersinfo('ltc_usd','18243800,18243801,18243644','0'))
+
+#print (u' 现货历史订单信息查询 ')
+#print (okcoinSpot.orderHistory('ltc_usd','0','1','2'))
+
+#print (u' 期货行情信息')
+#print (okcoinFuture.future_ticker('btc_usd','quarter'))
+
+#print (u' 期货市场深度信息')
+#print (okcoinFuture.future_depth('btc_usd','this_week','6'))
+
+#print (u'期货交易记录信息') 
+#print (okcoinFuture.future_trades('ltc_usd','this_week'))
+
+#print (u'期货指数信息')
+#print (okcoinFuture.future_index('ltc_usd'))
+
+#print (u'美元人民币汇率')
+#print (okcoinFuture.exchange_rate())
+
+#print (u'获取预估交割价') 
+#print (okcoinFuture.future_estimated_price('ltc_usd'))
+
+#print (u'获取全仓账户信息')
+#print (okcoinFuture.future_userinfo())
+
+#print (u'获取全仓持仓信息')
+#print (okcoinFuture.future_position('btc_usd','quarter'))  # works
+
+
+# |参数名|	参数类型|	必填|	描述|
+# | :-----    | :-----   | :-----    | :-----   |
+# |symbol|String|是|btc\_usd   ltc\_usd    eth\_usd    etc\_usd    bch\_usd|
+# |contract\_type|String|是|合约类型: this\_week:当周   next\_week:下周   quarter:季度|
+# |api_key|String|是|用户申请的apiKey|
+# |sign|String|是|请求参数的签名|
+# |price|String|是|价格|
+# |amount|String|是|委托数量|
+# |type|String|是|1:开多 2:开空 3:平多 4:平空|
+# |match_price|String|否|是否为对手价 0:不是    1:是   ,当取值为1时,price无效|
+# |lever_rate|String|否|杠杆倍数，下单时无需传送，系统取用户在页面上设置的杠杆倍数。且“开仓”若有10倍多单，就不能再下20倍多单|
+#future_trade(self,symbol,contractType,price='',amount='',tradeType='',matchPrice='',leverRate='') tradeType=2 best match price
+#print (u'期货下单')
+#print (okcoinFuture.future_trade('btc_usd','quarter','','1','1','1','10')) # works
+
+def open_quarter_sell_rate(symbol, contract, amount, price='', lever_rate='10'):
+    return okcoinFuture.future_trade(symbol, contract, '', amount, '2',
+                                     '1', '10')
+
+def close_quarter_sell_rate(symbol, contract, amount, price='', lever_rate='10'):
+    return okcoinFuture.future_trade(symbol, contract, '', amount, '4',
+                                     '1', '10')
+
+def open_quarter_buy_rate(symbol, contract, amount, price='', lever_rate='10'):
+    return okcoinFuture.future_trade(symbol, contract, '', amount, '1',
+                                     '1', '10')
+
+def close_quarter_buy_rate(symbol, contract, amount, price='', lever_rate='10'):
+    return okcoinFuture.future_trade(symbol, contract, '', amount, '3',
+                                     '1', '10')
+
+#print (u'期货批量下单')
+#print (okcoinFuture.future_batchTrade('ltc_usd','this_week','[{price:0.1,amount:1,type:1,match_price:0},{price:0.1,amount:3,type:1,match_price:0}]','20'))
+
+#print (u'期货取消订单')
+#print (okcoinFuture.future_cancel('ltc_usd','this_week','47231499'))
+
+#print (u'期货获取订单信息')
+#print (okcoinFuture.future_orderinfo('ltc_usd','this_week','47231812','0','1','2'))
+
+def quarter_orderinfo(symbol, order_id):
+    return okcoinFuture.future_orderinfo(symbol,'quarter',order_id,'0','1','2')
+
+#print (u'期货逐仓账户信息')
+#print (json.loads(okcoinFuture.future_userinfo_4fix()))
+
+#print (u'期货逐仓持仓信息')
+#print (json.loads(okcoinFuture.future_position_4fix('ltc_usd','quarter', '1')))
+
+#print (quarter_auto_bond('ltc_usd'), quarter_auto_balance('ltc_usd'))
+#os.sys.exit(0)
+
+def figure_out_symbol_info(path):
+    start_pattern = 'ok_sub_future'
+    end_pattern = '_kline_'
+    start = path.index(start_pattern) + len(start_pattern)
+    end = path.index(end_pattern)
+    # print (path[start:end])
+    return path[start:end]
+
+# {'result': True, 'holding': [{'buy_price_avg': 176.08158274, 'symbol': 'eth_usd', 'lever_rate': 10, 'buy_available': 0, 'contract_id': 201906280020041, 'sell_risk_rate': '99.36', 'buy_amount': 0, 'buy_risk_rate': '1,000,000.00', 'profit_real': -1.847e-05, 'contract_type': 'quarter', 'sell_flatprice': '178.453', 'buy_bond': 0, 'sell_profit_lossratio': '-0.66', 'buy_flatprice': '0.000', 'buy_profit_lossratio': '0.00', 'sell_amount': 1, 'sell_bond': 0.00615942, 'sell_price_cost': 162.388, 'buy_price_cost': 176.08158274, 'create_date': 1552656509000, 'sell_price_avg': 162.388, 'sell_available': 1}]}
+# if current order is permit to issue
+def check_holdings_profit(symbol, contract, direction):
+    holding=json.loads(okcoinFuture.future_position_4fix(symbol, contract, '1'))
+    if holding['result'] != True:
+        return 0
+    if len(holding['holding']) == 0:
+        return 0
+    # print (holding['holding'])
+    for data in holding['holding']:
+        if data['symbol'] == symbol:
+            if data['%s_amount' % direction] == 0 :
+                return 0
+            else :
+                return data['%s_profit_lossratio' % direction]
+    return 0
+
+order_infos = {'usd_btc':'btc_usd',
+               'usd_ltc':'ltc_usd',
+               'usd_eth':'eth_usd',
+               'usd_eos':'eos_usd',                              
+               'usd_bch':'bch_usd',
+               'sell':{'open':open_quarter_sell_rate,
+                       'close':close_quarter_sell_rate},
+               'buy':{'open':open_quarter_buy_rate,
+                      'close':close_quarter_buy_rate}}
+
+def issue_order_now(symbol, contract, direction, amount, action):
+    print (symbol, direction, amount, action)
+    raw_result = order_infos[direction][action](symbol, contract, amount)
+    result = json.loads(raw_result)
+    #print (result)
+    if result['result'] == False:
+        return False
+    order_id = str(result['order_id']) # no exceptions, means successed
+    #print (order_id)
+    order_info = json.loads(quarter_orderinfo(symbol, order_id))
+    print (order_info)
+
+def issue_order_now_conditional(symbol, contract, direction, amount, action, must_positive=True):
+    if must_positive == True and check_holdings_profit(symbol, contract, direction) <= 0:
+        return
+    return issue_order_now(symbol, contract, direction, amount, action)
+
+def issue_quarter_order_now(symbol, direction, amount, action):
+    print ('issue quarter order')
+    issue_order_now(symbol, 'quarter', direction, amount, action)
+
+def issue_quarter_order_now_conditional(symbol, direction, amount, action, must_positive=True):
+    print ('issue quarter order conditional')
+    issue_order_now_conditional(symbol, 'quarter', direction, amount, action, must_positive)
+
+def issue_thisweek_order_now(symbol, direction, amount, action):
+    print ('issue quarter order')
+    issue_order_now(symbol, 'this_week', direction, amount, action)
+
+def issue_thisweek_order_now_conditional(symbol, direction, amount, action, must_positive=True):
+    print ('issue quarter order conditional')
+    issue_order_now_conditional(symbol, 'this_week', direction, amount, action, must_positive)
 
 # apikey = 'e2625f5d-6227-4cfd-9206-ffec43965dab'
 # secretkey = "27BD16FD606625BCD4EE6DCA5A8459CE"
