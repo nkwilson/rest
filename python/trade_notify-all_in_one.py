953f11cfb5b6bb13b5a87a2c4677d23d1599637e
nkwilson <nkwilson@sina.com>
2019-07-23 22:52:40 +0800

    disable last_decision_logic and increase price resoltion to .4f

--- python/trade_notify-all_in_one.py
+++ python/trade_notify-all_in_one.py
@@ -772,7 +772,6 @@ names_tit2tat = ['trade_file',
                  'open_price',
                  'open_cost',
                  'open_greedy',
-                 'last_decision_logic',
                  'quarter_amount',
                  'thisweek_amount_pending',
                  'last_balance',
@@ -825,15 +824,15 @@ def try_to_trade_tit2tat(subpath):
         reverse_follow_dir = ''
         print ('') # add an empty line
         if trade_file == '':
-            print ('%9.3f' % close, '-')
+            print ('%9.4f' % close, '-')
         elif trade_file.endswith('.sell') == True: # sell order
             l_dir = 'sell'
             reverse_follow_dir = 'buy'
-            print ('%8.3f' % -close, '%9.3f' % open_price, l_dir, 'gate %9.3f' % open_start_price)
+            print ('%8.3f' % -close, '%9.4f' % open_price, l_dir, 'gate %9.4f' % open_start_price)
         elif trade_file.endswith('.buy') == True: # buy order
             l_dir = 'buy'
             reverse_follow_dir = 'sell'
-            print ('%9.3f' % close, '%8.3f' % -open_price, l_dir, 'gate %9.3f' % open_start_price)
+            print ('%9.4f' % close, '%8.3f' % -open_price, l_dir, 'gate %9.4f' % open_start_price)
         if close == 0: # in case read failed
             return
         if True:
