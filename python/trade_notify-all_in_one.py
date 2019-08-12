d282edc3284409b3c31f12182473993b24a42f0b
nkwilson <nkwilson@sina.com>
2019-08-12 23:36:50 +0800

    optimize open logic

--- python/trade_notify-all_in_one.py
+++ python/trade_notify-all_in_one.py
@@ -976,16 +976,19 @@ def try_to_trade_tit2tat(subpath):
                                     thisweek_amount_pending = 0;
                             issue_quarter_order_now_conditional(symbol, reverse_follow_dir, 0, 'close', False)
                         elif greedy_action == 'open': # yes, open action pending
-                            issue_quarter_order_now(symbol, l_dir, thisweek_amount, 'open')
+                            issue_open = True
                             if thisweek_amount_pending != 0:
-                                l_amount = issue_quarter_order_now_conditional(symbol, l_dir, thisweek_amount_pending, 'close')
-                                if thisweek_amount_pending >= l_amount: # is ok
-                                    thisweek_amount_pending -= l_amount
+                                l_amount = issue_quarter_order_now_conditional(symbol, l_dir, thisweek_amount_pending - thisweek_amount, 'close')
+                                thisweek_amount_pending -= l_amount
+                                if thisweek_amount_pending > 0:
+                                    issue_open = False
+                            if issue_open == True:
+                                issue_quarter_order_now(symbol, l_dir, thisweek_amount, 'open')
+                                thisweek_amount_pending += thisweek_amount
                             # first close current order
                             issue_quarter_order_now_conditional(symbol, reverse_follow_dir, 0, 'close')
                             # secondly open new order
                             issue_quarter_order_now(symbol, reverse_follow_dir, thisweek_amount / 2, 'open')
-                            thisweek_amount_pending += thisweek_amount
                     if issuing_close == True:
                         globals()['signal_close_order_with_%s' % l_dir](l_index, trade_file, close)
                         issue_quarter_order_now_conditional(symbol, l_dir, 0, 'close', False)
