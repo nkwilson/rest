75cfda4d446a3d29629a84da35a1d10210d9f22a
nkwilson <nkwilson@sina.com>
2019-08-16 23:34:51 +0800

    convert list to tuple, for json's sake

--- python/trade_notify-all_in_one.py
+++ python/trade_notify-all_in_one.py
@@ -284,6 +284,7 @@ def issue_order_now_conditional(symbol, contract, direction, amount, action, mus
     l_reverse=orders_holding[direction]['reverse']
     print (holding)
     if len(holding) > 1:
+        holding = [tuple(x) for x in holding]
         holding.sort(reverse=l_reverse)
     if must_positive == False:
         if amount == 0:
