c2a3fe744ffd1d7b912df11dfb164f372a4e950e
nkwilson <nkwilson@sina.com>
2020-01-04 09:41:58 +0800

    Update trade_notify-all_in_one.py
    should close it unconditionally

--- python/trade_notify-all_in_one.py
+++ python/trade_notify-all_in_one.py
@@ -1187,7 +1187,7 @@ def try_to_trade_tit2tat(subpath, guard=False):
                                 issue_quarter_order_now_conditional(symbol, reverse_follow_dir, 0, 'close', False)
                         elif greedy_action == 'open': # yes, open action pending
                             if greedy_count < 1.0: # must bigger than 1
-                                issue_quarter_order_now_conditional(symbol, l_dir, thisweek_amount - 1, 'close') # left 1 in case empty holding
+                                issue_quarter_order_now(symbol, l_dir, thisweek_amount - 1, 'close') # forced, left 1 in case empty holding
                             else:
                                 if forward_greedy: 
                                     issue_quarter_order_now(symbol, l_dir, thisweek_amount, 'open')
